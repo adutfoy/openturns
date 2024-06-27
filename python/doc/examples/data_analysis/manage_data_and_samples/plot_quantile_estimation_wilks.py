f8788fcb3836691514dc8be54c52d388e50bd13b
Anne Dutfoy <anne.dutfoy@m4x.org>
2024-06-27 22:17:51 +0200

    Improved Wilks doc: theory, API, example

--- python/doc/examples/data_analysis/manage_data_and_samples/plot_quantile_estimation_wilks.py
+++ python/doc/examples/data_analysis/manage_data_and_samples/plot_quantile_estimation_wilks.py
@@ -1,56 +1,26 @@
 """
-Estimate Wilks and empirical quantile
-=====================================
+Estimate a confidence interval of a particular quantile
+=======================================================
 """
 
 # %%
-# In this example we want to evaluate a particular quantile, with the empirical estimator or the Wilks one, from a sample of a random variable.
+# In this example, we detail two ways to estimate a confidence interval of the quantile of order :math:`\alpha` of a scalar random
+# variable :math:`X`. Both ways use the order statistics to estimate:
 #
+# - an asympotic confidence interval of level :math:`\beta`,
+# - an exact upper bounded confidence interval of level :math:`\beta`.
 #
-# Let us suppose we want to estimate the quantile :math:`q_{\alpha}` of order :math:`\alpha` of the variable :math:`Y`:
-# :math:`P(Y \leq q_{\alpha}) = \alpha`, from the sample :math:`(Y_1, ..., Y_n)`
-# of size :math:`n`, with a confidence level equal to :math:`\beta`.
-#
-# We note :math:`(Y^{(1)}, ..., Y^{(n)})` the sample where the values are sorted in ascending order.
-# The empirical estimator, noted :math:`q_{\alpha}^{emp}`, and its confidence interval, are defined by the expressions:
-#
-# .. math::
-#     \left\{
-#      \begin{array}{lcl}
-#        q_{\alpha}^{emp} & = & Y^{(E[n\alpha])} \\
-#        P(q_{\alpha} \in [Y^{(i_n)}, Y^{(j_n)}]) & = & \beta \\
-#        i_n & = & E[n\alpha - a_{\alpha}\sqrt{n\alpha(1-\alpha)}] \\
-#        i_n & = & E[n\alpha + a_{\alpha}\sqrt{n\alpha(1-\alpha)}]
-#      \end{array}
-#     \right\}
-#
-# The Wilks estimator, noted :math:`q_{\alpha, \beta}^{Wilks}`, and its confidence interval, are defined by the expressions:
-#
-# .. math::
-#   \left\{
-#   \begin{array}{lcl}
-#     q_{\alpha, \beta}^{Wilks} & = & Y^{(n-i)} \\
-#     P(q_{\alpha}  \leq q_{\alpha, \beta}^{Wilks}) & \geq & \beta \\
-#     i\geq 0 \, \, /  \, \, n \geq N_{Wilks}(\alpha, \beta,i)
-#   \end{array}
-#   \right\}
-#
-# Once the order :math:`i` has been chosen, the Wilks number :math:`N_{Wilks}(\alpha, \beta,i)` is evaluated,
-# thanks to the static method :math:`ComputeSampleSize(\alpha, \beta, i)` of the :class:`~openturns.Wilks` object.
-#
-# In the example, we want to evaluate a quantile :math:`\alpha = 95\%`,
-# with a confidence level of :math:`\beta = 90\%` thanks to the :math:`4` th maximum of
-# the ordered sample (associated to the order :math:`i = 3` ).
-#
-# Be careful: :math:`i=0` means that the Wilks estimator is the maximum of the sample:
-# it corresponds to the first maximum of the sample.
+# In this example, we consider the quantile of order :math:`\alpha = 95\%`,
+# with a confidence level of :math:`\beta = 90\%`.
 
 # %%
 import openturns as ot
 import math as m
-import openturns.viewer as viewer
 
 ot.Log.Show(ot.Log.NONE)
+
+# %%
+# We consider a random vector which is the output of a model and an input distribution.
 
 # %%
 model = ot.SymbolicFunction(["x1", "x2"], ["x1^2+x2"])
@@ -60,49 +30,81 @@ inputDist = ot.Normal([0.0, 0.0], R)
 inputDist.setDescription(["X1", "X2"])
 inputVector = ot.RandomVector(inputDist)
 
-# Create the output random vector Y=model(X)
+# Create the output random vector
 output = ot.CompositeRandomVector(model, inputVector)
 
 # %%
-# Quantile level
-alpha = 0.95
+# We define the order :math:`\alpha` of the quantile and the confidence level :math:`\beta`.
 
-# Confidence level of the estimation
+# %%
+alpha = 0.95
 beta = 0.90
 
 # %%
-# Get a sample of the variable
-N = 10**4
-sample = output.getSample(N)
-graph = ot.UserDefined(sample).drawCDF()
-view = viewer.View(graph)
+# We generate a sample of the variable.
 
 # %%
-# Empirical Quantile Estimator
-empiricalQuantile = sample.computeQuantile(alpha)
+n = 10**4
+sample = output.getSample(n)
 
-# Get the indices of the confidence interval bounds
-aAlpha = ot.Normal(1).computeQuantile((1.0 + beta) / 2.0)[0]
-min_i = int(N * alpha - aAlpha * m.sqrt(N * alpha * (1.0 - alpha)))
-max_i = int(N * alpha + aAlpha * m.sqrt(N * alpha * (1.0 - alpha)))
-# print(min_i, max_i)
+# %%
+# We get the empirical estimator of the quantile of order :math:`\alpha` which is the
+# :math:`\lfloor N\alpha \rfloor` -th order statistics evaluated on
+# the sample.
+
+# %%
+empiricalQuantile = sample.computeQuantile(alpha)
+print(empiricalQuantile)
+
+# %%
+# The asymptotic confidence interval of level :math:`\beta` is :math:`\left[ X_{(i_n)}, X_{(j_n)}\right]`
+# such that:
+#
+# .. math::
+#
+#    i_n & = \lfloor n \alpha - \sqrt{n}a_{\beta}\sqrt{\alpha(1-\alpha)}\rfloor\\
+#    j_n & = \lfloor n \alpha + \sqrt{n}a_{\beta}\sqrt{\alpha(1-\alpha)}\rfloor
+#
+# where  :math:`a_{\beta}` is the quantile of order :math:`(1-\beta/2)` of the standard normal distribution.
+#
+# Then we have:
+#
+# .. math::
+#
+#     \lim\limits_{n \rightarrow +\infty} \Prob{x_{\alpha} \in \left[ X_{(i_n,n)}, X_{(j_n,n)}\right]} = 1-\beta
+
+# %%
+a_beta = ot.Normal(1).computeQuantile((1.0 + beta) / 2.0)[0]
+i_n = int(n * alpha - a_beta * m.sqrt(n * alpha * (1.0 - alpha)))
+j_n = int(n * alpha + a_beta * m.sqrt(n * alpha * (1.0 - alpha)))
+print(i_n, j_n)
 
 # Get the sorted sample
 sortedSample = sample.sort()
 
-# Get the Confidence interval of the Empirical Quantile Estimator [infQuantile, supQuantile]
-infQuantile = sortedSample[min_i - 1]
-supQuantile = sortedSample[max_i - 1]
+# Get the asymptotic confidence interval :math:`\left[ X_{(i_n)}, X_{(j_n)}\right]`
+# Care: the index in the sorted sample is :math:`i_n-1` and :math:`j_n-1`
+infQuantile = sortedSample[i_n - 1]
+supQuantile = sortedSample[j_n - 1]
 print(infQuantile, empiricalQuantile, supQuantile)
 
 # %%
-# Wilks number
-i = N - (min_i + max_i) // 2  # compute wilks with the same sample size
-wilksNumber = ot.Wilks.ComputeSampleSize(alpha, beta, i)
-print("wilksNumber =", wilksNumber)
+# The empirical quantile was estimated with the :math:`\lfloor \sampleSize\alpha \rfloor` -th order statistics evaluated on
+# the sample of size :math:`\sampleSize`.
+# We define :math:`i = \sampleSize-\lfloor \sampleSize\alpha \rfloor` and we evaluate the minimum sample size :math:`\tilde{\sampleSize}` that
+# ensures that the :math:`(\tilde{\sampleSize}-i)` order statistics is greater than :math:`x_{\alpha}` with the confidence :math:`\beta`.
 
 # %%
-# Wilks Quantile Estimator
+i = n - int(n * alpha)
+minSampleSize = ot.Wilks.ComputeSampleSize(alpha, beta, i)
+print(minSampleSize)
+
+# %%
+# Here we directly ask for the evaluation of the upper bounded confidence interval:
+# the Wilks class estimates the previous minimum sample size, generates a
+# sample with that size and extracts the empirical quantile of order :math:`(\tilde{\sampleSize}-i)`.
+
+# %%
 algo = ot.Wilks(output)
-wilksQuantile = algo.computeQuantileBound(alpha, beta, i)
-print("wilks Quantile 0.95 =", wilksQuantile)
+upperBoundQuantile = algo.computeQuantileBound(alpha, beta, i)
+print(upperBoundQuantile)
